8496350716f15183c6d1162572d9a48121f06ba4
iulikchip <iulikchip@gmail.com>
2016-06-16 16:59:50 +0300

    Add exercises 31 - 40. Add Google Python Class exercises.

--- PygLatin/ex31.py
+++ PygLatin/ex31.py
@@ -21,3 +21,13 @@ elif door == "2":
     print "1. Blueberries."
     print "2. Yellow jacket clothespins."
     print "3. Understanding revolvers yelling melodies."
+
+    insanity = raw_input("> ")
+
+    if insanity == "1" or insanity == "2":
+        print "Your body survives powered by a mind of jello.  Good job!"
+    else:
+        print "The insanity rots your eyes into a pool of muck.  Good job!"
+
+else:
+    print "You stumble around and fall on a knife and die.  Good job!"
